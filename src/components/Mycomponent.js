8062def452ed3bb77e7a59bef78fa3d74f5ed5fd
iamnubie <nguyenhongminh952@gmail.com>
2024-06-12 22:34:55 +0700

    learn22

--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -1,51 +1,15 @@
 import React from "react";
+import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
 
-    state = {
-        name: 'Minh',
-        address: 'Hoi Dan IT',
-        age: 22
-    };
 
-    handleClick(event) {
-        console.log("click me on my button")
-        //merge State => react class
-        this.setState({
-            name: 'Eric',
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-        // this.setState({
-
-        // })
-    }
-
-    handleOnMover(event) {
-        //console.log(event.pageX)
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state)
-    }
     //jsx
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm {this.state.age}
-                <form onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <input
-                        type="text"
-                        onChange={(event) => this.handleOnChangeInput(event)}
-                    />
-                    <button>Submit</button>
-                </form>
+
+                <UserInfor />
             </div>
         );
 
