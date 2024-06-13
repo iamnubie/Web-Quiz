14ce69cdad425253819fc8be68c904ea671a1ede
iamnubie <nguyenhongminh952@gmail.com>
2024-06-13 16:37:22 +0700

    learn26

--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import UserInfor from "./UserInfor";
+import AddUserInfor from "./AddUserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
@@ -11,13 +11,24 @@ class MyComponent extends React.Component {
         ]
     }
 
+    handleAddNewUser = (userObj) => {
+        console.log(">>>check data from parent:", userObj)
+        this.setState({
+            //listUsers: [userObj, ...this.state.listUsers]
+            listUsers: [...this.state.listUsers, userObj]
+        })
+    }
+
     //jsx
     render() {
         //DRY:don't repeat yourself
         return (
             <div>
 
-                <UserInfor />
+                <AddUserInfor
+                    handleAddNewUser={this.handleAddNewUser}
+
+                />
                 <br /><br />
                 <DisplayInfor
                     listUsers={this.state.listUsers}
