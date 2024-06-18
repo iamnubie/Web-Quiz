3a415e8064133925aba94acda7b54e12f4f37333
iamnubie <nguyenhongminh952@gmail.com>
2024-06-18 21:56:05 +0700

    api cap nhat user (update)-64

--- src/components/Admin/Content/TableUser.js
+++ src/components/Admin/Content/TableUser.js
@@ -21,15 +21,18 @@ const TableUser = (props) => {
                         listUsers.map((item, index) => {
                             return (
                                 <tr key={`table-users-${index}`}>
-                                    <td >{item.id}</td>
+                                    <td>{item.id}</td>
                                     <td>{item.username}</td>
                                     <td>{item.email}</td>
                                     <td>{item.role}</td>
-                                    <tb>
+                                    <td>
                                         <button className="btn btn-secondary">View</button>
-                                        <button className="btn btn-warning mx-3">Update</button>
+                                        <button
+                                            className="btn btn-warning mx-3"
+                                            onClick={() => props.handleClickBtnUpdate(item)}
+                                        >Update</button>
                                         <button className="btn btn-danger">Delete</button>
-                                    </tb>
+                                    </td>
                                 </tr>
                             )
                         })
