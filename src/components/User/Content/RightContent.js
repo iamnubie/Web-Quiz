5a7fcaee87cf9792bda08e22214ca15f4f589216
iamnubie <nguyenhongminh952@gmail.com>
2024-06-29 14:46:21 +0700

    select question-105

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -1,16 +1,59 @@
+import CountDown from "./CountDown";
+import { useRef } from "react";
+
 const RightContent = (props) => {
+    const refDiv = useRef([]);
+
     const { dataQuiz } = props;
-    console.log('dataquiz: ', dataQuiz)
+
+    const onTimeUp = () => {
+        props.handleFinishQuiz();
+    }
+
+    const getClassQuestion = (index, question) => {
+        //check answered
+        if (question && question.answers.length > 0) {
+            let isAnswered = question.answers.find(a => a.isSelected === true);
+            if (isAnswered) {
+                return 'question selected'
+            }
+        }
+        return "question";
+    }
+    const handleClickQuestion = (question, index) => {
+        if (refDiv.current) {
+            refDiv.current.forEach(item => {
+                if (item && item.className === "question clicked") {
+                    item.className = "question"
+                }
+            })
+        }
+        if (question && question.answers.length > 0) {
+            let isAnswered = question.answers.find(a => a.isSelected === true);
+            if (isAnswered) {
+                return;
+            }
+        }
+        refDiv.current[index].className = "question clicked";
+        props.setIndex(index);
+    }
     return (
         <>
             <div className="main-timer">
-                10:10
+                <CountDown
+                    onTimeUp={onTimeUp}
+                />
             </div>
             <div className="main-question">
                 {dataQuiz && dataQuiz.length > 0
                     && dataQuiz.map((item, index) => {
                         return (
-                            <div className="question">{index + 1}</div>
+                            <div
+                                key={`question-abc-${index}`}
+                                className={getClassQuestion(index, item)}
+                                onClick={() => handleClickQuestion(item, index)}
+                                ref={element => refDiv.current[index] = element}
+                            >{index + 1}</div>
                         )
                     })
                 }
