eea0948e39c45ca3477b9462b88dc43687312d2c
iamnubie <nguyenhongminh952@gmail.com>
2024-06-29 17:44:45 +0700

    logout-107

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,15 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink, useNavigate } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../services/apiService';
+import { toast } from 'react-toastify';
+import { doLogout } from '../../redux/action/userAction';
 
 const Header = () => {
     const isAuthenticated = useSelector(state => state.user.isAuthenticated);
     const account = useSelector(state => state.user.account);
+    const dispatch = useDispatch();
     const navigate = useNavigate();
     const handleLogin = () => {
         navigate('/login')
@@ -15,7 +19,17 @@ const Header = () => {
     const handleRegister = () => {
         navigate('/register');
     }
-
+    const handleLogOut = async () => {
+        let rs = await logout(account.email, account.refresh_token);
+        if (rs && rs.EC === 0) {
+            //clear data redux
+            dispatch(doLogout());
+            navigate('/login');
+        } else {
+            toast.error(rs.EM);
+        }
+        console.log('check res: ', rs)
+    }
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
@@ -40,8 +54,8 @@ const Header = () => {
                             </>
                             :
                             <NavDropdown title="Settings" id="basic-nav-dropdown">
-                                <NavDropdown.Item >Log out</NavDropdown.Item>
                                 <NavDropdown.Item >Profile</NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => handleLogOut()}>Log out</NavDropdown.Item>
                             </NavDropdown>
                         }
                     </Nav>
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -102,6 +102,11 @@ const getQuizWithQA = (quizId) => {
 const postUpsertQA = (data) => {
     return axios.post(`api/v1/quiz-upsert-qa`, { ...data });
 }
+const logout = (email, refresh_token) => {
+    return axios.post('api/v1/logout', {
+        email, refresh_token
+    });
+}
 export {
     postCreateNewUser, getAllUsers,
     putUpdateUser, deleteUser, getUserWithPaginate,
@@ -109,5 +114,5 @@ export {
     postSubmitQuiz, postCreateNewQuiz, getAllQuizForAdmin,
     putUpdateQuizForAdmin, deleteQuizForAdmin, postCreateNewQuestionForQuiz,
     postCreateNewAnswerForQuestion, postAssignQuiz, getQuizWithQA,
-    postUpsertQA
+    postUpsertQA, logout
 }
